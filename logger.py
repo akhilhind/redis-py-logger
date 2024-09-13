2f8fee83dea67a2731ca45260b1ac6b19c6266a1
akhil <akhilas.singh@gmail.com>
2024-09-13 20:41:02 +0530

    minor change regardint adding redis to logger

--- logger.py
+++ logger.py
@@ -1,5 +1,8 @@
+from redis_cache import RedisDB
+
 class Logger:
     def __init__(self) -> None:
-        self.data_to_export = None
+        self.database_to_export = None
         self.log_file = None
         self.log_color_scheme = None
+        self.client = RedisDB()
